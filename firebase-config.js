f33bb821cd447fe77f6d408329a258bb80937adb
afqalmrft <mktbtafqalmrft@gmail.com>
2026-05-04 11:08:12 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,14 @@
-// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAe6ihk4E_SHuuPtoEnVebnuS9JdFw-FWU",
-  authDomain: "afaq-almarefa.firebaseapp.com",
-  projectId: "afaq-almarefa",
-  storageBucket: "afaq-almarefa.firebasestorage.app",
-  messagingSenderId: "851811909716",
-  appId: "1:851811909716:web:26ac06a353161066d6edc1"
+  apiKey: "AIzaSyCyE6UbwF1HYqcLb7vV2p2CnBBVgaWBMlA",
+  authDomain: "afqalmrft-e1913.firebaseapp.com",
+  projectId: "afqalmrft-e1913",
+  storageBucket: "afqalmrft-e1913.firebasestorage.app",
+  messagingSenderId: "871199543253",
+  appId: "1:871199543253:web:387907520495191e92f75c",
+  measurementId: "G-8EQD69ZCKY"
 };
 
 const app = initializeApp(firebaseConfig);
